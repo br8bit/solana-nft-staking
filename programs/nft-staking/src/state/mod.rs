583acce595330cbc7ff816a8859ee85da365a65e
br8bit <>
2025-02-06 21:23:06 +0400

    update mod

--- programs/nft-staking/src/state/mod.rs
+++ programs/nft-staking/src/state/mod.rs
@@ -1,5 +1,7 @@
-pub mod config;
-pub mod user_config;
+pub mod stake_account;
+pub mod stake_config;
+pub mod user_account;
 
-pub use config::*;
-pub use user_config::*;
+pub use stake_account::*;
+pub use stake_config::*;
+pub use user_account::*;

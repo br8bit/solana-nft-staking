ab2a3c63ec4922c9a6ef94930b71df25f2eccc0f
br8bit <>
2025-02-06 21:38:45 +0400

    add unstake and claim

--- programs/nft-staking/src/contexts/mod.rs
+++ programs/nft-staking/src/contexts/mod.rs
@@ -1,8 +1,10 @@
+pub mod claim;
 pub mod init_config;
 pub mod init_users;
 pub mod stake;
 pub mod unstake;
 
+pub use claim::*;
 pub use init_config::*;
 pub use init_users::*;
 pub use stake::*;
--- programs/nft-staking/src/lib.rs
+++ programs/nft-staking/src/lib.rs
@@ -3,14 +3,39 @@ use anchor_lang::prelude::*;
 declare_id!("9AtqAo7BUPvB96Vx4L1yv8We32WGVHxj7p8LFRpCAM3w");
 
 pub mod contexts;
+pub mod errors;
 pub mod state;
+
+use contexts::*;
 
 #[program]
 pub mod nft_staking {
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize_config(
+        ctx: Context<InitConfig>,
+        points_per_stake: u8,
+        max_stake: u8,
+        freeze_period: u64,
+    ) -> Result<()> {
+        ctx.accounts
+            .init(points_per_stake, max_stake, freeze_period, &ctx.bumps)
+    }
+
+    pub fn initialize_user(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.initialize_user(&ctx.bumps)
+    }
+
+    pub fn stake(ctx: Context<Stake>) -> Result<()> {
+        ctx.accounts.stake(&ctx.bumps)
+    }
+
+    pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
+        ctx.accounts.unstake()
+    }
+
+    pub fn claim(ctx: Context<Claim>) -> Result<()> {
+        ctx.accounts.claim()
     }
 }
